55cf68a755da6eb67af39e4c24a7cc8c7f19437a
Pranav Ramesh <laphatize@protonmail.com>
2024-07-29 19:23:48 -0400

    -

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -35,7 +35,7 @@ export default function Home() {
         className="flex items-center justify-center min-h-screen"
       >
         <div className="text-center bg-neutral-800 p-10 rounded-lg">
-        <img src="./darklogo.png" alt="CTFGuide Logo" width={100} className='mx-auto' />
+        <img src="https://preview.ctfguide.com/darkLogo.png" alt="CTFGuide Logo" width={100} className='mx-auto' />
 
         <h1 className='text-white text-3xl font-semibold'>Platform Maintenance</h1>
           <h1 className="mb-2 text-2xl  text-white">
@@ -45,7 +45,7 @@ export default function Home() {
             We will be back at July 31st 11:59 PM EST.
           </p>
           <br></br>
-          <button href="https://discord.gg/bH6gu3HCFF" className='bg-blue-600 text-white px-10 py-2 rounded-lg'>Join the Discord</button>
+          <a href="https://discord.gg/bH6gu3HCFF" className='bg-blue-600 text-white px-10 py-2 rounded-lg'>Join the Discord</a>
    
       <p className='mt-4 text-white text-sm mt-10'>&copy; CTFGuide Corporation 2024</p>
         </div>
