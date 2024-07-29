c534442c83052e6e6be005f3f754aa29c3749e54
Pranav Ramesh <laphatize@protonmail.com>
2024-07-29 17:52:08 -0400

    feat: maintenance

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,6 @@
 import Head from 'next/head';
-import { Header } from '@/components/Header';
-import { Hero } from '@/components/home/Hero';
-import { Footer } from '@/components/Footer';
-import { FeaturesPanel } from '@/components/home/FeaturePanel';
-import { Stats } from '@/components/home/Stats';
-import { LearningPanel } from '@/components/home/LearningPanel';
-import { Enterprise } from '@/components/home/Enterprise';
-import { SecondaryFeatures } from '@/components/home/SecondaryFeatures';
-import GP  from '@/components/home/GP';
+
+
 export default function Home() {
   return (
     <>
@@ -36,30 +29,27 @@ export default function Home() {
     
 
 
-      <Hero />
-
-
-      <GP/>
-
-      <SecondaryFeatures/>
-
-
+      <br></br>
 
       <div
-        className="mx-auto mx-auto max-w-7xl px-6 py-20 text-center lg:px-8 hidden"
-        style={{ backgroundColor: '#212121' }}
+        className="flex items-center justify-center min-h-screen"
       >
-        <h1 className="mx-auto mb-8 text-center text-4xl font-semibold text-white">
-          What are you waiting for? Start hacking today!
-        </h1>
-        <a
-          href="./register"
-          className=" mx-auto rounded-lg bg-blue-600 px-10 py-2 text-center text-2xl text-white"
-        >
-          Create Account
-        </a>
+        <div className="text-center bg-neutral-800 p-10 rounded-lg">
+        <img src="/darklogo.png" alt="CTFGuide Logo" width={100} className='mx-auto' />
+
+        <h1 className='text-white text-3xl font-semibold'>Platform Maintenance</h1>
+          <h1 className="mb-2 text-2xl  text-white">
+            We're preparing for the launch of CTFGuide V2.
+          </h1>
+          <p className='text-white text-xl'>
+            We will be back at July 31st 11:59 PM EST.
+          </p>
+          <br></br>
+          <button className='bg-blue-600 text-white px-10 py-2 rounded-lg'>Join the Discord</button>
+   
+      <p className='mt-4 text-white text-sm mt-10'>&copy; CTFGuide Corporation 2024</p>
+        </div>
       </div>
-     <Footer />
     </>
   );
 }
